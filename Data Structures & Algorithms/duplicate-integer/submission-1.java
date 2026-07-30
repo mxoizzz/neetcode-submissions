772b10ff29dcd42fb94b968825a334b30b347b82
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-30 19:54:55 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for(int n : nums){
+            if(!seen.add(n)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
